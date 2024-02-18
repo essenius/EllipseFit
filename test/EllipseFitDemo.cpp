950a55fcd4c6f25a49f0de5c74626ead179cf706
essenius <essenius@users.noreply.github.com>
2024-02-18 21:33:32 +0100

    Added post build step to generate library ZIP and optimized example provisioning

--- test/EllipseFitDemo.cpp
+++ test/EllipseFitDemo.cpp
@@ -1,4 +1,4 @@
-// Copyright 2024 Rik Essenius
+// Copyright 2023-2024 Rik Essenius
 // 
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
 // except in compliance with the License. You may obtain a copy of the License at
@@ -13,46 +13,58 @@
 #include <CartesianEllipse.h>
 #include <MathUtils.h>
 
-#include <gtest/gtest.h>
-
 using namespace EllipseMath;
 
-namespace EllipseFitTest {
+void demoFit() {
+    // start the fitter
+    EllipseFit ellipseFit;
+    ellipseFit.begin();
 
-    // copy to examples/EllipseFitDemo when changed
+    // create test data generator
+    constexpr auto Center = Coordinate{ 1, 2 };
+    constexpr auto Radius = Coordinate{ 10, 6 };
+    constexpr auto XAxisAngle = Angle{ M_PI / 3 };
+    const auto inputEllipse = CartesianEllipse(Center, Radius, XAxisAngle);
+    const unsigned int pointsOnEllipse = EllipseFit::getSize();
 
-    TEST(EllipseFitDemo, Demo) {
-        // start the fitter
-        EllipseFit ellipseFit;
-        ellipseFit.begin();
-
-        // create test data generator
-        constexpr auto center = Coordinate{ 1, 2 };
-        constexpr auto radius = Coordinate{ 10, 6 };
-        constexpr auto angle = Angle{ M_PI / 3 };
-        const auto inputEllipse = CartesianEllipse(center, radius, angle);
-        const unsigned int pointsOnEllipse = EllipseFit::getSize();
-
-        // feed the fitter with half an ellipse
-        double currentAngle = 0;
-        const double delta = M_PI / pointsOnEllipse;
-        for (unsigned int i = 0; i < pointsOnEllipse; i++) {
-            Coordinate point = inputEllipse.getPointOnEllipseAtAngle(Angle{ currentAngle });
-            ellipseFit.addMeasurement(point);
-            currentAngle += delta;
-        }
-
-        // execute the fitting
-        if (!ellipseFit.bufferIsFull()) {
-            printf("  Expected buffer to be full, but it wasn't\n");
-        }
-        else {
-            const auto resultQuadraticEllipse = ellipseFit.fit();
-
-            // Result should be equal to the parameters of the test data generator
-            const auto result = CartesianEllipse(resultQuadraticEllipse);
-            printf("  Result:\n   Center (%f, %f)\n   Radius (%f, %f)\n   Angle %f\n", result.getCenter().x, result.getCenter().y, result.getRadius().x, result.getRadius().y, result.getAngle().value);
-        }
+    // feed the fitter with half an ellipse
+    double currentAngle = 0;
+    const double delta = M_PI / pointsOnEllipse;
+    for (unsigned int i = 0; i < pointsOnEllipse; i++) {
+        Coordinate point = inputEllipse.getPointOnEllipseAtAngle(Angle{ currentAngle });
+        ellipseFit.addMeasurement(point);
+        currentAngle += delta;
     }
 
+    // execute the fitting
+    if (!ellipseFit.bufferIsFull()) {
+        printf(" Expected buffer to be full, but it wasn't\n");
+    }
+    else {
+        const auto resultQuadraticEllipse = ellipseFit.fit();
+
+        // Result should be equal to the parameters of the test data generator
+        const auto result = CartesianEllipse(resultQuadraticEllipse);
+        printf(" Result:\n   Center (%f, %f)\n   Radius (%f, %f)\n   Angle %f\n",
+            result.getCenter().x, result.getCenter().y,
+            result.getRadius().x, result.getRadius().y,
+            result.getAngle().value);
+        // Result:
+        //   Center (1.000000, 2.000000)
+        //   Radius (10.000000, 6.000000)
+        //   Angle 1.047198
+
+    }
+}
+
+void setup() {
+#ifdef ESP32
+    Serial.begin(115200);
+    delay(500);
+#endif
+    demoFit();
+}
+
+void loop() {
+    // nothing to do, waiting forever.
 }
